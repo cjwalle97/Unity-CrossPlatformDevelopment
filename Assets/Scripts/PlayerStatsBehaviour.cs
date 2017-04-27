e73338983ae57c58a93f43b1d542737b08a1e62e
cjwalle97 <cjwalle97@gmail.com>
2017-04-27 12:36:56 -0500

    Update
    Update

---
+++ Assets/Scripts/PlayerStatsBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatsBehaviour : MonoBehaviour {
+
+    public int knowledge;
+    public int guts;
+    public int proficiency;
+    public int charm;
+    public int kindness;
+
+	// Use this for initialization
+	void Start () {
+        knowledge = 1;
+        guts = 1;
+        proficiency = 1;
+        charm = 1;
+        kindness = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
