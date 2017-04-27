d2289724b4d8e2b12377ceb77b7194b23d01e2a2
cjwalle97 <cjwalle97@gmail.com>
2017-04-27 17:11:51 -0500

    Update
    Update

--- Assets/Scripts/GutsBehaviour.cs
+++ Assets/Scripts/GutsBehaviour.cs
@@ -3,21 +3,9 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using RPGStats;
 
 public class GutsBehaviour: MonoBehaviour
-{
-
-    public int guts;
-    public Text level;
-
-    void Start()
-    {
-        guts = 1;
-    }
-
-    void Update()
-    {
-        string text = "Guts Level: " + guts;
-        level.text = text + Environment.NewLine;
-    }
+{ 
+   
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using RPGStats;
+
+public class Player : MonoBehaviour {
+
+    public class player
+    {
+        Stat playerHealth = new Stat("Health", 100);
+        Stat playerKnowledge = new Stat("Knowledge", 1);
+        Stat playerGuts = new Stat("Guts", 1);
+        Stat playerProficiency = new Stat("Proficiency", 1);
+        Stat playerCharm = new Stat("Charm", 1);
+        Stat playerKindness = new Stat("Kindness", 1);
+
+        Stats playerStats = new Stats();
+    }
+}
