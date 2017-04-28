18ba2fd08aabb7961db44fbb49e273b3b5daec62
cjwalle97 <cjwalle97@gmail.com>
2017-04-28 14:38:46 -0500

    Update
    Update

---
+++ Assets/Scripts/GridScripts/GridBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehaviour : MonoBehaviour {
+
+    public GameObject gridblocks;
+
+    public void MakeGrid() {
+        
+        for(int i = 0; i <= 3; i++)
+        {
+            for(int j = 0; j <= 3; j++)
+            {
+                
+            }
+        }
+    }
+}
