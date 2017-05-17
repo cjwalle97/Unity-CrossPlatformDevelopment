1ad6a7719e69065fe429a892a0f2929e366f34b8
cjwalle97 <cjwalle97@gmail.com>
2017-05-17 16:53:30 -0500

    Update
    Update

---
+++ Assets/Scripts/PlayerHealthChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Events;
+
+[System.Serializable]
+public class PlayerHealthChange : UnityEvent {
+
+}
+
+[System.Serializable]
+public class PlayerHealthChangeWithInt : UnityEvent<int> { }
+[System.Serializable]
+public class PlayerChange : UnityEvent<TestPlayerBehaviour.PlayerInfo> { }
---
+++ Assets/Scripts/PlayerTextBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class PlayerTextBehaviour : MonoBehaviour {
+    public Text texttoChange;
+
+    void ChangeText()
+    {
+        texttoChange.text = FindObjectOfType<TestPlayerBehaviour>().ThePlayer.Health.ToString();
+    }
+}
